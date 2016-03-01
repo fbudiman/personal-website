a34323f31db91e4ec60f8e93f537d34dda6f971f
Fran Budiman <fbudiman14@hotmail.com>
2016-03-01 13:59:43 -0800

    restarting git repo for personal website

---
+++ js/MainController.js
@@ -0,0 +1,18 @@
+angular.module('PersonalWebsite')
+  .controller('MainController', MainController);
+
+function MainController() {
+
+  this.welcome = "Hello. It's me."
+  this.description = "Web developer / Foodie / Aspiring World Traveler"
+  this.about = "Who, me?"
+  this.projects = "Recent Work:"
+  this.contact = "Hello world! Say hello back:"
+
+  this.images = {
+    one: "images/project1-1.png",
+    two: "images/project2-1.png",
+    three: "images/project3-1.png"
+  }
+
+}
---
+++ js/app.js
@@ -0,0 +1,37 @@
+angular
+  .module('PersonalWebsite', ['ui.router'])
+  .config(MainRouter)
+
+function MainRouter($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('welcome', {
+      url: '/',
+      templateUrl: 'views/welcome.html'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: 'views/about.html'
+    })
+    .state('projects', {
+      url: '/projects',
+      templateUrl: 'views/projects.html'
+    })
+    .state('contact', {
+      url: '/contact',
+      templateUrl: 'views/contact.html'
+    })
+    .state('project-one', {
+      url: '/project-one',
+      templateUrl: 'views/project-one.html'
+    })
+    .state('project-two', {
+      url: '/project-two',
+      templateUrl: 'views/project-two.html'
+    })
+    .state('project-three', {
+      url: '/project-three',
+      templateUrl: 'views/project-three.html'
+    })
+
+    $urlRouterProvider.otherwise('/')
+}
